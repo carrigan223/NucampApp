85b7005bdd9b0bab4955375a7890dd4ed0a147bd
Andrew Carrigan <carrigan223@gmail.com>
2020-06-01 15:21:45 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,64 +1,52 @@
 import React, { Component } from 'react';
-import { render } from '@testing-library/react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { CAMPSITES } from '../shared/campsites';
-
-
 
 class CampsiteInfo extends Component {
-
-
-renderCampsite(campsite) {
-    return(
-        <div className="col-md-5 m-1">
-            <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-        </div>
-    )
-} 
-
-renderComments(comments) {
-    if (comments) {
+    renderCampsite(campsite) {
         return(
             <div className="col-md-5 m-1">
-                <h4>Comments</h4>
-                {comments.map(comment => 
-                    <div key={comment.id}>
-                        <div>{comment.text}</div>
-                        <div>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
-                        <br />
-                    </div>)}
+                <Card>
+                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
             </div>
         )
-    } else {
+    } // renderCampsite method is showing the selected campsite info
+    renderComments(comments) {
+        if (comments) {
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => 
+                        <div key={comment.id}>
+                            <p>
+                                {comment.text}
+                                <br />
+                                -- {comment.author}, 
+                                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>)}
+                </div>
+            )
+        } else {
+            return <div></div>
+        };
+    }// renderComments method is showing the selected campgrounds comments
+    render() {
+        if (this.props.campsite) {
+        return <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+        } else {
         return <div></div>
-    };
-}
-
-
-
-
-render() {
-    if (this.props.campsite) {
-       return <div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-              </div>
-    } else {
-       return <div></div>
-        }
-    };
-
-}
-
-
-
-
-
+            }
+        };
+}// this render function containing renderCampsites and renderComments causes the campsite component to render upon click
 
 export default CampsiteInfo;
