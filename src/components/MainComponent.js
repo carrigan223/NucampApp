7d1d266fc1b5acd68bdc7495ca6395e59d7e3adf
Andrew Carrigan <carrigan223@gmail.com>
2020-06-06 11:27:27 -0700

    about us workshop 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -56,6 +57,7 @@ class Main extends Component {
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={About} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
